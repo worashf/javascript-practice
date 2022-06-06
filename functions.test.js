687d3dc4098aed1dbec9e573946f095e5e6e4baf
worashf <uppert83@gmail.com>
2022-06-06 22:44:17 +0300

    add toBeNull, falsy and truthy test

--- functions.test.js
+++ functions.test.js
@@ -9,3 +9,24 @@ test('Adds 3 + 3 to equal 6', () => {
   test("Adds 2 +5 not to be 8",()=>{
       expect(functions.add(2,5)).not.toBe(8)
   })
+
+  //test with to be null matcher
+  test("Should be null",()=>{
+      expect(functions.notNull()).toBeNull()
+  })
+// falsy
+test("Shoulld be falsy",()=>{
+    expect(functions.checkValue(null)).toBeFalsy()
+})
+// falsy
+test("Shoulld be falsy",()=>{
+    expect(functions.checkValue(0)).toBeFalsy()
+})
+// falsy
+test("Shoulld be falsy",()=>{
+    expect(functions.checkValue(undefined)).toBeFalsy()
+})
+// truthy
+test("Shoulld be falsy",()=>{
+    expect(functions.checkValue(1)).toBeTruthy()
+})
