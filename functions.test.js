1281d386458e7f7623add74479b70b6871c1c4b0
worashf <uppert83@gmail.com>
2022-06-06 21:53:19 +0300

    add test with not tobe

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,9 @@ const functions =require("./functions")
 test('Adds 3 + 3 to equal 6', () => {
     expect(functions.add(3, 3)).toBe(6);
   });
+
+
+  //test with not to be
+  test("Adds 2 +5 not to be 8",()=>{
+      expect(functions.add(2,5)).not.toBe(8)
+  })
