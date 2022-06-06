200e43eb91db8c6e7c26cfe86ab07d23b78afd6f
worashf <uppert83@gmail.com>
2022-06-06 22:53:44 +0300

    add createUser function to test toEqual

--- functions.js
+++ functions.js
@@ -2,7 +2,15 @@
 const functions ={
 add:(num1,num2)=>num1+num2,
 notNull:()=>null,
-checkValue:x =>x
+checkValue:x =>x,
+createUser: ()=>{
+
+return user={
+        firstName:"worash",
+        lastName:"abocherugn"
+    }
+   
+}
     
 };
 module.exports =functions;
